ec0949725af0c9a772a617a6fa0ae4530c98acf4
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-06-19 17:41:56 -0700

    remove non-official JWasm

--- lib/Assemblers/JWasm_old/src/H/data.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef DATA_H
-#define DATA_H
-
-extern ret_code data_dir( int, struct asm_tok[], struct asym * );
-extern ret_code InitStructuredVar( int, struct asm_tok[], const struct dsym *, struct expr *, struct asym * );
-
-#endif
