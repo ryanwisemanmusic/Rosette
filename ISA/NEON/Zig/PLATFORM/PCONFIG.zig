42c5953393bb8d90ff5170215b9b1f65eef76300
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-07-03 00:17:34 -0700

    PLATFORM .inc files

---
+++ ISA/NEON/Zig/PLATFORM/PCONFIG.zig
@@ -0,0 +1,5 @@
+pub const family = "PLATFORM";
+pub const path = "PLATFORM/PCONFIG.inc";
+pub const source = @embedFile("../../PLATFORM/PCONFIG.inc");
+pub const x86_path = "PLATFORM/PCONFIG.inc";
+pub const target_isa = "arm64_neon";
