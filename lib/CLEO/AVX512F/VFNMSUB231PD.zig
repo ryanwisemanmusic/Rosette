12ae61c3ca94bdbf715eb24835f5ecd67aa6d328
Ryan Wiseman <ryanwisemanmusic@gmail.com>
2026-07-13 22:56:22 -0700

    CLEO: New AVX512F Instructions

---
+++ lib/CLEO/AVX512F/VFNMSUB231PD.zig
@@ -0,0 +1,23 @@
+const Impl = @import("../wrapper.zig").BinaryInstruction(.{
+    .name = "VFNMSUB231PD",
+    .family = "FUSED",
+    .source_path = "ISA/x86/FUSED/VFNMSUB231PD.inc",
+    .required_feature = .avx512f,
+    .max_width_bits = 512,
+    .element_bits = 64,
+    .operation = .fnms_pd,
+    .supports_masking = true,
+    .supports_broadcast = true,
+});
+
+pub const meta = Impl.meta;
+pub const plan = Impl.plan;
+pub const safety = Impl.safety;
+pub const validate = Impl.validate;
+pub const execute = Impl.execute;
+pub const executeAccumulate = Impl.executeAccumulate;
+pub const executeMasked = Impl.executeMasked;
+pub const executeMaskedImmediate = Impl.executeMaskedImmediate;
+pub const executeAccumulateMasked = Impl.executeAccumulateMasked;
+pub const move = Impl.move;
+pub const movMask = Impl.movMask;
